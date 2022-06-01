96da86a9c2eeb2154693f14ed74a0df9baad070d
Muhammad Fahad <fa19bsse0014@maju.edu.pk>
2022-06-01 09:03:37 +0500

    MFY auto commit at 01/06/2022 09:03:11

---
+++ 6) Test Driven Development(TDD)_BM/Class Task/ATMTest-20211117T073145Z-001/ATMTest/ATMTest/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ATMClass;
+
+namespace ATMTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ATMTestForWithdrawal()
+        {
+            ATM atm = new ATM();
+            Param parame = new Param();
+            parame.amount = 50000;
+            parame.choice = 2;
+            parame.withdraw = 10000;
+            parame.pin = 123;
+            Assert.AreEqual(atm.BankTransaction(parame), "Your current amount now is 40000");
+        }
+
+        [TestMethod]
+        public void ATMTestForDeposit()
+        {
+            ATM atm = new ATM();
+            Param parame = new Param();
+            parame.amount = 50000;
+            parame.choice = 3;
+            parame.deposit = 10000;
+            parame.pin = 123;
+            Assert.AreEqual(atm.BankTransaction(parame), "Your amount now has been deposited successfully 60000");
+        }
+    }
+}
