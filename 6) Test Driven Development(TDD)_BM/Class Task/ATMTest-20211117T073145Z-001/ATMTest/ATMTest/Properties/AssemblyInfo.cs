5ea4b04005d76d8761a5c262d9f8eca25ca21f4d
Muhammad Fahad <fa19bsse0014@maju.edu.pk>
2022-06-01 09:03:37 +0500

    MFY auto commit at 01/06/2022 09:03:11

---
+++ 6) Test Driven Development(TDD)_BM/Class Task/ATMTest-20211117T073145Z-001/ATMTest/ATMTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ATMTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ATMTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a6a4cb92-f2ee-47e2-80b0-2c9b2877cbda")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
