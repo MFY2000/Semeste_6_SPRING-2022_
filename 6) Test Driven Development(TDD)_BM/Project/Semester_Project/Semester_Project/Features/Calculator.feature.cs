214d7f21d98b0d92239620859f87411e6e827f7b
Muhammad Fahad <fa19bsse0014@maju.edu.pk>
2022-06-03 17:17:23 +0500

    new new

--- 6) Test Driven Development(TDD)_BM/Project/Semester_Project/Semester_Project/Features/Calculator.feature.cs
+++ 6) Test Driven Development(TDD)_BM/Project/Semester_Project/Semester_Project/Features/Calculator.feature.cs
@@ -10,7 +10,7 @@
 // ------------------------------------------------------------------------------
 #region Designer generated code
 #pragma warning disable
-namespace Semester_Project.Features
+namespace Calculator_Testing.Features
 {
     using TechTalk.SpecFlow;
     using System;
--- 6) Test Driven Development(TDD)_BM/Project/Semester_Project/Semester_Project/obj/Debug/net6.0/NUnit.AssemblyHooks.cs
+++ 6) Test Driven Development(TDD)_BM/Project/Semester_Project/Semester_Project/obj/Debug/net6.0/NUnit.AssemblyHooks.cs
@@ -9,13 +9,13 @@ using global::System.Runtime.CompilerServices;
 
 [GeneratedCode("SpecFlow", "3.9.40")]
 [SetUpFixture]
-public class Semester_Project_NUnitAssemblyHooks
+public class Calculator_Testing_NUnitAssemblyHooks
 {
     [OneTimeSetUp]
     [MethodImpl(MethodImplOptions.NoInlining)]
     public void AssemblyInitialize()
     {
-        var currentAssembly = typeof(Semester_Project_NUnitAssemblyHooks).Assembly;
+        var currentAssembly = typeof(Calculator_Testing_NUnitAssemblyHooks).Assembly;
 
         TestRunnerManager.OnTestRunStart(currentAssembly);
     }
@@ -24,7 +24,7 @@ public class Semester_Project_NUnitAssemblyHooks
     [MethodImpl(MethodImplOptions.NoInlining)]
     public void AssemblyCleanup()
     {
-        var currentAssembly = typeof(Semester_Project_NUnitAssemblyHooks).Assembly;
+        var currentAssembly = typeof(Calculator_Testing_NUnitAssemblyHooks).Assembly;
 
         TestRunnerManager.OnTestRunEnd(currentAssembly);
     }
